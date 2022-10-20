be7807a55e8078ebcdce407afcf89e414159423b
Al-Amin Hossain <alaminnishat@gmail.com>
2022-10-20 22:46:59 +0600

    correct answer show toast added

--- src/Components/Question/Question.js
+++ src/Components/Question/Question.js
@@ -16,11 +16,35 @@ const Question = ({ questions, count }) => {
       document.getElementById(option).classList.add("bg-danger", "text-light");
     }
   };
+
   return (
-    <div className="border rounded my-1 text-start p-3">
-      <div className="question">
-        {count}
-        <div dangerouslySetInnerHTML={{ __html: question }}></div>
+    <div className="border rounded my-4 text-start p-5">
+      <div className="d-flex justify-content-between ">
+        <div className="question d-flex ">
+          {`Question No: ${count}. `}
+          <div
+            className="ms-3"
+            dangerouslySetInnerHTML={{ __html: question }}
+          ></div>
+        </div>
+        <div>
+          <button
+            className="show-answer"
+            onClick={() => toast.success(`Answer is : ${correctAnswer}`)}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="20"
+              height="20"
+              fill="currentColor"
+              class="bi bi-eye-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M10.5 8a2.5 2.5 0 1 1-5 0 2.5 2.5 0 0 1 5 0z" />
+              <path d="M0 8s3-5.5 8-5.5S16 8 16 8s-3 5.5-8 5.5S0 8 0 8zm8 3.5a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7z" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div className="options">
         {options.map((option) => (
@@ -48,7 +72,11 @@ const Question = ({ questions, count }) => {
               theme="dark"
             />
 
-            <label className="ms-2" htmlFor={option}>
+            <label
+              className="ms-2"
+              htmlFor={option}
+              onClick={() => checkAns(option)}
+            >
               {option}
             </label>
           </div>
