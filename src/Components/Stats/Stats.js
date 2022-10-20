234f19e55b8b22763997bb92a564b08e9ec8bf62
Al-Amin Hossain <al-aminhossain@Al-Amins-MacBook-Air.local>
2022-10-21 01:45:15 +0600

    Stats Responsive Checked

--- src/Components/Stats/Stats.js
+++ src/Components/Stats/Stats.js
@@ -3,12 +3,12 @@ import { useLoaderData } from "react-router-dom";
 import {
   BarChart,
   Bar,
-  CartesianGrid,
   XAxis,
   YAxis,
   Tooltip,
   ResponsiveContainer,
 } from "recharts";
+import "./Stats.css";
 
 const Stats = () => {
   const { data } = useLoaderData();
@@ -18,13 +18,15 @@ const Stats = () => {
     <div className="my-5 container-fluid">
       <h2 className="mb-5">Quiz Stats</h2>
 
-      <BarChart className="mx-auto" width={600} height={400} data={data}>
-        <Bar dataKey="total" fill="#0d6efd" />
+      <ResponsiveContainer className="mx-auto" width="75%" height={400}>
+        <BarChart data={data}>
+          <Bar dataKey="total" fill="#0d6efd" />
 
-        <XAxis dataKey="name" tick="name" />
-        <YAxis></YAxis>
-        <Tooltip></Tooltip>
-      </BarChart>
+          <XAxis dataKey="name" tick="name" />
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+        </BarChart>
+      </ResponsiveContainer>
     </div>
   );
 };
